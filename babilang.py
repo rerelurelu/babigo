31a9792e5dc1e8c920d5efd1ffbfaf05065c962f
snova <awaawapurin@gmail.com>
2021-03-16 01:27:29 +0900

    main file

---
+++ babilang.py
@@ -0,0 +1,26 @@
+from hiragana_converter import goo_converter, kakasi_converter
+from babi_converter import babi_converter
+
+
+if __name__ == '__main__':
+    sentence = '勝つしかねぇので'
+    hiragana = goo_converter(sentence)
+    output = babi_converter(hiragana)
+
+    # goo api converter
+    print('<< goo API >>')
+    print(f'input -> {sentence}')
+    print(f'hira -> {hiragana}')
+    print(f'result -> {output}')
+
+    # A line to make it easier to see
+    print('-'*40)
+
+    # pykakasi converter
+    hiragana = kakasi_converter(sentence)
+    output = babi_converter(hiragana)
+
+    print('<< Pykakasi >>')
+    print(f'input -> {sentence}')
+    print(f'hira -> {hiragana}')
+    print(f'result -> {output}')

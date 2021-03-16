eb4acbfca68ca5a1c325218bc689c59417379b37
snova <awaawapurin@gmail.com>
2021-03-17 02:06:02 +0900

    updated

--- tests/test_babi_converter.py
+++ tests/test_babi_converter.py
@@ -8,18 +8,9 @@ from converter.babi_converter import babi_converter
 
 
 test_data = pd.read_csv('tests/test_data/test_data.csv')
+length = len(test_data)
 
 
 def test_babi_converter() -> None:
-    data = [[sentence[1], sentence[2]] for sentence in test_data.itertuples()]
-
-    assert babi_converter(data[0][0]) == data[0][1]
-    assert babi_converter(data[2][0]) == data[2][1]
-    assert babi_converter(data[3][0]) == data[3][1]
-    assert babi_converter(data[4][0]) == data[4][1]
-    assert babi_converter(data[5][0]) == data[5][1]
-    assert babi_converter(data[6][0]) == data[6][1]
-    assert babi_converter(data[7][0]) == data[7][1]
-    assert babi_converter(data[8][0]) == data[8][1]
-    assert babi_converter(data[9][0]) == data[9][1]
-    assert babi_converter(data[10][0]) == data[10][1]
+    for data in test_data.itertuples():
+        assert babi_converter(data[2]) == data[3]
